2d6505c07439ccd4f537a5dca066349037d6dfc5
Sandeep <sandeepsk15892>
2022-11-16 17:44:03 +0530

    code

--- lib/jobs/jobs_screen.dart
+++ lib/jobs/jobs_screen.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:job_clone_app_flutter/user_state.dart';
+import 'package:job_clone_app_flutter/search/search_job.dart';
 
+import '../util/constants.dart';
 import '../widgets/bottom_navigation_bar.dart';
 
 class JobsScreen extends StatefulWidget {
@@ -13,23 +14,108 @@ class JobsScreen extends StatefulWidget {
 
 class _JobsScreenState extends State<JobsScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  String? jobCategoryFilter;
+
+  void _showTextCategoryDialog({required Size size}) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.white,
+            title: const Text(
+              "Job category",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+            content: SizedBox(
+              width: size.width * 0.9,
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: jobCategoryList.length,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          jobCategoryFilter = jobCategoryList[index];
+                        });
+                        Navigator.canPop(context)
+                            ? Navigator.pop(context)
+                            : null;
+                        print("jobCategoryList:: ${jobCategoryList[index]}");
+                      },
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.arrow_right_alt_outlined,
+                            color: Colors.grey,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              jobCategoryList[index],
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.grey),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                },
+                child: const Text(
+                  "Close",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    jobCategoryFilter = null;
+                  });
+                  Navigator.canPop(context) ? Navigator.pop(context) : null;
+                },
+                child: const Text(
+                  "Cancel Filter",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Container(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: [
-          Colors.deepOrange.shade300,
-          Colors.blueAccent,
-        ],
-        begin: Alignment.centerLeft,
-        end: Alignment.centerRight,
-        stops: const [0.2, 0.9],
-      ),),
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepOrange.shade300,
+            Colors.blueAccent,
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.2, 0.9],
+        ),
+      ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        bottomNavigationBar: BottomNavBar(indexNum: 0,),
+        bottomNavigationBar: BottomNavBar(
+          indexNum: 0,
+        ),
         appBar: AppBar(
           flexibleSpace: Container(
             decoration: BoxDecoration(
@@ -46,6 +132,32 @@ class _JobsScreenState extends State<JobsScreen> {
           ),
           title: const Text("Job Screen"),
           centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SearchJobScreen(),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+            ),
+          ],
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            onPressed: () {
+              _showTextCategoryDialog(size: size);
+            },
+            icon: const Icon(
+              Icons.filter_list_rounded,
+              color: Colors.black,
+            ),
+          ),
         ),
         body: Container(),
       ),
---
+++ lib/search/search_job.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SearchJobScreen extends StatefulWidget {
+  const SearchJobScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchJobScreen> createState() => _SearchJobScreenState();
+}
+
+class _SearchJobScreenState extends State<SearchJobScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.deepOrange.shade300,
+            Colors.blueAccent,
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: const [0.2, 0.9],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.deepOrange.shade300,
+                  Colors.blueAccent,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                stops: const [0.2, 0.9],
+              ),
+            ),
+          ),
+          title: const Text("Search Jobs"),
+          centerTitle: true,
+        ),
+        body: Center(),
+      ),
+    );
+  }
+}
